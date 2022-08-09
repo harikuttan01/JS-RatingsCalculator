f4c6c6d2be083110f7fecdfcac8c92ab498e0d91
Hareendran VR <hareendran.vr@lntinfotech.com>
2022-08-09 18:43:36 +0530

    demo complete

---
+++ js/ratings.js
@@ -0,0 +1,22 @@
+const collect_ratings = () =>{
+    var ratings = new Object();
+    ratings.count = 0;
+    ratings.sum = 0;
+    ratings.average = 0;
+    let rating = 0;
+    var elements = document.querySelectorAll('rating');
+    elements.forEach((element)=>{
+       rating = parseInt(element.id.replace('star','')); 
+       ratings.count += parseInt(element.value)
+
+       ratings.sum += rating*parseInt(element.value)
+
+       if(ratings.count != 0){
+        ratings.average = ratings.sum/ratings.count;
+        ratings.average.toFixed(2);
+       }
+    })
+
+}
+
+const ratings = document.addEventListener('change',collect_ratings(), document.querySelector('average').value = ratings.average);
